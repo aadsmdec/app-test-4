3e7afc23d09436f54b027f45f28bb8614ac1adbe
Thibault Zanini <noreply>
2014-03-14 16:23:16 +0000

    Add component penguin

---
+++ ui/penguin.reel/penguin.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/penguin.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Penguin
+ * @extends Component
+ */
+exports.Penguin = Component.specialize(/** @lends Penguin# */ {
+    constructor: {
+        value: function Penguin() {
+            this.super();
+        }
+    }
+});
